ee5a7a2174fc08499d05a6f9abc8ba536a8c066e
ryislov <46944741+ryislov@users.noreply.github.com>
2019-04-09 14:11:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,3 @@
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, options);
   });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.slider').slider();
-  });
